2d189380ad5e0e90dc78aedac13bdc8e30cf73ba
Avinna07 <avinna123@gmail.com>
2024-01-24 21:36:56 -0600

    project creation

---
+++ photography-portfolio/src/app/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return(
+        <section>
+            <div className="grid grid-cols-1 lg:grid-cols-12">
+                <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Photography Portfolio</h1>
+
+                <p className="text-[#ADB7BE] text-lg lg:text">
+                    Hi, how is this looking?
+                </p>
+            </div>
+
+        </section>
+    )
+};
+export default HeroSection;
