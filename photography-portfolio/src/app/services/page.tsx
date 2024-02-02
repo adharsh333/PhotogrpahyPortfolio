e4b713d9b92d5f483e9a176dcac5d187b029d22a
adharsh333 <adharshadhikari333@gmail.com>
2024-02-02 02:04:57 -0600

    removed cluttered code for card

--- photography-portfolio/src/app/services/page.tsx
+++ photography-portfolio/src/app/services/page.tsx
@@ -34,42 +34,18 @@ const Services = () => {
 
 
     <div className="px-48 py-32 z-10">
-                <div className='grid grid-rows-3 gap-8'>
-                <div className="backdrop-blur-3xl lg:bg-white/30 py-6 rounded-3xl">
-    <Card
-      isFooterBlurred
-      radius="lg"
-      className="border-solid border-rounded-lg px-8 hover:blur-lg"
-    >
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        
-      </CardHeader>
-      <div>
+                <div className='grid grid-rows-3 gap-8 '>
+                <div className="backdrop-blur-3xl lg:bg-white/30 px-8 py-6 rounded-3xl hover:blur-lg">
       <Image
         alt="Woman listing to music"
-        className="object-cover rounded-xl float-left"
-        height={500}
+        className="object-cover rounded-xl float-center"
+        height={1000}
         src={image2}
         width={1000}
       />
-      <h4 className="font-bold text-transparent absolute text-9xl bg-clip-text bg-gradient-to-r from-gray-200  to-gray-200 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">PHOTOGRPAHY</h4>
-       </div>
-      <CardFooter className="before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">### Photography</p>
-        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-         <Camera/>
-        </Button>
-      </CardFooter>
-    </Card>
+      <h4 className="font-bold text-transparent absolute text-9xl bg-clip-text bg-gradient-to-r from-gray-200  to-gray-200 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">PHOTOGRPAHY</h4>    
 </div>
-<div className="backdrop-blur-3xl bg-white/30 rounded-3xl">
-    <Card
-      isFooterBlurred
-      radius="lg"
-      className="border-solid border-rounded-lg px-8 hover:blur-lg"
-    >
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-      </CardHeader>
+<div className="backdrop-blur-3xl bg-white/30 px-8 py-6 rounded-3xl hover:blur-lg">
       <div>
       <Image
         alt="Woman listing to music"
@@ -80,39 +56,18 @@ const Services = () => {
       />
     <h4 className="font-bold text-transparent absolute text-9xl bg-clip-text bg-gradient-to-r from-gray-200  to-gray-200 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">VIDEOGRAPHY</h4>
       </div>
-      <CardFooter className="before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">### Photography</p>
-        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-         <Camera/>
-        </Button>
-      </CardFooter>
-    </Card>
 </div>
-<div className="backdrop-blur-3xl bg-white/60 rounded-3xl">
-    <Card
-      isFooterBlurred
-      radius="lg"
-      className="border-solid border-rounded-lg px-8 hover:blur-lg"
-    >
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-      </CardHeader>
+<div className="backdrop-blur-3xl bg-white/60 px-8 py-6 rounded-3xl hover:blur-lg">
       <div>
       <Image
         alt="Woman listing to music"
         className="object-cover rounded-xl float-left"
-        height={500}
+        height={1000}
         src={image2}
         width={1000}
       />
      <h4 className="font-bold text-transparent absolute text-9xl bg-clip-text bg-gradient-to-r from-gray-200  to-gray-200 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">EDITING</h4>
         </div>
-      <CardFooter className="before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">### Photography</p>
-        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-         <Camera/>
-        </Button>
-      </CardFooter>
-    </Card>
 </div>
     </div>
     </div>
